76d0570f6dbb47de2b7f6f3a6489a7a72aa7423b
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-03-30 15:11:15 -0400

    Update: wrong name function

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -3,12 +3,14 @@ const database = require('../models')
 class PessoaController {
     static async pegarTodasPessoas(req, res){
         try{
-            const todasAsPessoas = await database.Pessoas.findAl();
+            const todasAsPessoas = await database.Pessoas.findAll();
             return res
-            .status(200)
-            .json(todasAsPessoas);
+                .status(200)
+                .json(todasAsPessoas);
         } catch(error){
             return res.status(500).json(error.message)
         }
     }
 }
+
+module.exports = PessoaController
