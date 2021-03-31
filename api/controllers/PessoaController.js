db37c8a5df2f0872d0c75b8aaccf9f60935d3105
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-03-31 13:15:58 -0400

    Add: creating CRUD operators on application

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,6 +1,17 @@
 const database = require('../models')
 
 class PessoaController {
+
+    static async criarPessoa(req, res){
+        const novaPessoa = req.body
+        try{
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(201).json(novaPessoaCriada)
+        } catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
     static async pegarTodasAsPessoas(req, res){
         try{
             const todasAsPessoas = await database.Pessoas.findAll();
@@ -14,12 +25,54 @@ class PessoaController {
 
     static async pegarUmaPessoa(req, res){
         const { pessoaId } = req.params
-        const umaPessoa = await database.Pessoas.findOne({
-            where: {
-                id: Number(pessoaId)
-            }
-        })
-        return res.status(200).json(umaPessoa)
+
+        try{
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(pessoaId)
+                }
+            })
+            return res.status(200).json(umaPessoa)
+        } catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async alterarUmaPessoa(req, res){
+        const { pessoaId } = req.params;
+        const alteracoes = req.body
+        try{
+            await database.Pessoas.update(
+                alteracoes,
+                {
+                    where: {
+                        id: Number(pessoaId)
+                    }
+                }
+            )
+            const pessoaAlterada = await database.Pessoas.findOne({
+                where: {
+                    id: Number(pessoaId)
+                }
+            })
+            return res.status(200).json(pessoaAlterada)
+        } catch(error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async deletarUmaPessoa(req, res){
+        const { pessoaId } = req.params
+        try{
+            await database.Pessoas.destroy({
+                where: {
+                    id: Number(pessoaId)
+                }
+            })
+            return res.status(204).json()
+        } catch( error){
+            return res.status(500).json(error.message)
+        }
     }
 }
 
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,7 +3,10 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 
+router.post('/pessoas', PessoaController.criarPessoa)
 router.get('/pessoas', PessoaController.pegarTodasAsPessoas)
 router.get('/pessoas/:pessoaId', PessoaController.pegarUmaPessoa)
+router.put('/pessoas/:pessoaId', PessoaController.alterarUmaPessoa)
+router.delete('/pessoas/:pessoaId', PessoaController.deletarUmaPessoa)
 
 module.exports = router
